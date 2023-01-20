bd2d6bc6b3536af55517f48fa06a87bd6e2bc5b4
Jackson Utsch <jutechs@gmail.com>
2023-01-20 15:04:13 -0700

    Use org dependency

--- Package.swift
+++ Package.swift
@@ -18,8 +18,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(
-      url: "https://github.com/JacksonUtsch/URLSessionBackport.git", .upToNextMajor(from: "0.2.1"))
+    .package(url: "https://github.com/hatchmed/URLSessionBackport.git", .upToNextMajor(from: "0.2.1")),
   ],
   targets: [
     .target(
